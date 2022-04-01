f583e6c2d820bb0601e0c4f48bb894f320a1fc2f
josep3121 <josepb.ayala@gmail.com>
2022-04-01 15:13:09 -0500

    Correccion en rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:simple_dialog/inicio.dart';
+import 'package:simple_dialog/datos.dart';
+//import 'package:simple_dialog/inicio.dart';
+import 'package:simple_dialog/menu.dart';
+import 'package:simple_dialog/miscursos.dart';
+import 'package:simple_dialog/redes.dart';
+
+import 'contactos.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,9 +14,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Inicio()
+      initialRoute: "/",
+      routes: {
+        "/": (BuildContext context) => Menu(),
+        "/contactos": (BuildContext context) => MisContactos(),
+        "/redes": (BuildContext context) => RedesSociales(),
+        "/datospersonales": (BuildContext context) => DatosPersonales(),
+        "/miscursos": (BuildContext context) => MisCursos(),
         
-      
+      },
     );
   }
 }
